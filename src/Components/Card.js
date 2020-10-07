0656df6538a4e7f9c637837858ea06b102546ab8
atheen <aatheen.ds@gmail.com>
2020-10-07 14:21:07 +0300

    Done with comparing the cards and keeping a card flipped up if they match

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,29 +1,34 @@
 import React, { useState } from "react";
+import cardBack from "../images/CardBack.jpg";
 import ReactCardFlip from "react-card-flip";
 
-import cardBack from "../images/CardBack.jpg";
-
-const Card = ({ card }) => {
+const Card = ({ card, checkFlipped }) => {
   const [flipped, changeFlip] = useState(false);
 
   const handleFlip = () => {
-    changeFlip(!flipped);
-  };
+    if (flipped !== true) {
+      changeFlip(true);
 
+      checkFlipped({ id: card.id, changeFlip: changeFlip });
+    }
+  }; 
   return (
     <div className="col-3 my-1">
       <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
         <img
           className="mx-auto"
           src={cardBack}
+          //   used percentages instead of pixels to be responsive with the screen size
           height="100%"
           width="100%"
           key="front"
           onClick={() => handleFlip()}
         />
+
         <img
           className="mx-auto"
           src={card.front}
+          //   used percentages instead of pixels to be responsive with the screen size
           height="100%"
           width="100%"
           key="back"
